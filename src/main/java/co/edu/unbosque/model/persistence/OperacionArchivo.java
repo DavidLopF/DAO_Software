186640b13039042e6a6eacd56c6d80eb1531ed69
xD <david.lopez@puntored.co>
2022-08-28 12:19:40 -0500

    save arraylist in binay file

--- src/main/java/co/edu/unbosque/model/persistence/OperacionArchivo.java
+++ src/main/java/co/edu/unbosque/model/persistence/OperacionArchivo.java
@@ -1,27 +1,56 @@
 package co.edu.unbosque.model.persistence;
 
+
+import org.w3c.dom.CDATASection;
+
 import java.io.*;
+import java.util.ArrayList;
 
 public class OperacionArchivo {
 
     private File archivo;
-    private FileOutputStream flujoSalida ;
     private ObjectOutputStream flujoObjeto;
+
+    private ObjectInputStream flujoObjetoEntrada;
+
+    private ArrayList<Persona> personas;
 
 
     public OperacionArchivo() throws IOException {
-        archivo = new File("src/main/resources/archivo.dat");
-        flujoSalida = new FileOutputStream(archivo);
-        flujoObjeto = new ObjectOutputStream(flujoSalida);
+        personas = new ArrayList<Persona>();
+        archivo = new File("src/main/resources/personas.dat");
     }
 
-    public boolean savePerson(Persona persona) throws IOException {
+    public boolean savePerson(Persona persona) throws IOException, ClassNotFoundException {
         try {
-            flujoObjeto.writeObject(persona);
+            leerPersonas();
+            flujoObjeto = new ObjectOutputStream(new FileOutputStream(archivo));
+            personas.add(persona);
+            flujoObjeto.writeObject(personas);
+            flujoObjeto.close();
             return true;
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("error in cath " + e);
             return false;
         }
     }
+
+    public void leerPersonas() throws IOException, ClassNotFoundException {
+        try{
+            flujoObjetoEntrada = new ObjectInputStream(new FileInputStream(archivo));
+            personas = (ArrayList<Persona>) flujoObjetoEntrada.readObject();
+            System.out.println(personas.toString());
+            flujoObjetoEntrada.close();
+        }catch (IOException e){
+            System.out.println("error in cath " + e);
+        }catch (ClassNotFoundException e){
+            System.out.println("error in cath " + e);
+        }
+    }
+
+
+
+
+
+
 }
